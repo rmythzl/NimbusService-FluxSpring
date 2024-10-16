eeb51c2e3f5a7452481c37fba99fe372d1606b8b
Rian Gabriel <SUPORTERMYTH@GMAIL.COM>
2024-10-16 00:58:46 -0300

    Initial commit

---
+++ src/main/java/RM/NimbusService/FluxSpring/NimbusServiceApplication.java
@@ -0,0 +1,13 @@
+package RM.NimbusService.FluxSpring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimbusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NimbusServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/RM/NimbusService/FluxSpring/controller/UserController.java
@@ -0,0 +1,47 @@
+package RM.NimbusService.FluxSpring.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import RM.NimbusService.FluxSpring.model.User;
+import RM.NimbusService.FluxSpring.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.getUserById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
